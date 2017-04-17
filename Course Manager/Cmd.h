9e36023ca0f5486239084b6a160e27dd4dc9b554
林范 <linfan255@163.com>
2017-04-17 17:07:57 +0800

    Create Cmd.h

---
+++ Course Manager/Cmd.h
@@ -0,0 +1,11 @@
+enum CourseCmd {
+	Cmd_printHelp = 0,
+	Cmd_printCourse = 1,
+	Cmd_printCourseNum = 2,
+	Cmd_printLongName = 3,
+	Cmd_removeLastCourse = 4,
+	Cmd_removeByName = 5,
+	Cmd_removeById = 6,
+	Cmd_addCourse = 7,
+	Cmd_exit = 8,
+};
