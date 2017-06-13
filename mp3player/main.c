db26e7fd7b9ba1b11f70c33ecafed934e02ba58d
林范 <linfan255@163.com>
2017-06-13 11:15:32 +0800

    Update main.c

--- mp3player/main.c
+++ mp3player/main.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 #include "player.h"
+#include "net.h"
 
 void showMenu(char *filename) {
 	system("clear");
 	printf("\n\n\n\n\n");
 	printf("                                  0--exit\n");
-	printf("                             1--play/stop\n");
+	printf("                                1--play/stop\n");
 	printf("                                  2--next\n");
 	printf("                                  3--prev\n");
+	printf("                          4--download song from server\n");
+	printf("                                 5--song list\n");
 	printf("\n");
 	if(filename != NULL)
 		printf("                        ==========playing %s===========\n", filename);
@@ -26,6 +29,7 @@ int main(int argc, char **argv) {
 	curr = head;
 
 	isPlaying = 0, init = 1;	//标识当前是否正在播放
+	int songid;
 
 	showMenu(NULL);
 	for(;;) {
@@ -33,6 +37,7 @@ int main(int argc, char **argv) {
 		  perror("signal alarm");
 		op = -1;
 		scanf("%d", &op);
+		getchar();
 
 		switch(op) {
 			case 0:
@@ -85,6 +90,33 @@ int main(int argc, char **argv) {
 				showMenu(curr->songName);
 				Play(curr->songName);
 				break;
+				
+			case 4:
+				showMenu(curr->songName);
+				getSongFromServ();
+				break;
+
+			case 5:
+				showMenu(curr->songName);
+				print();
+				break;
+
+			case 6:
+				printf("input the song id:");
+				scanf("%d", &songid);
+				getchar();
+				while(curr->id != songid) {
+					curr = curr->next;
+					if(curr == NULL)	curr = head;
+				}
+				if(isPlaying) {
+					kill(pid, SIGKILL);
+					isPlaying = 0;
+				}
+				isPlaying = 1;
+				Play(curr->songName);
+				break;
+
 
 			case 9:
 				printf("pid2 = %d,pid = %d,isPlaying = %d,init = %d\n", pid2, pid, isPlaying, init);
